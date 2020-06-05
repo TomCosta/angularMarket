375b90f162ae44274e0cedd657798bc7df566c81
Everton Costa <sys.everton@gmail.com>
2020-06-04 21:01:09 -0300

    Iniciando produtos.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularMarket';
+
+  title = 'Angular Mark';
+
+  produtos = [
+    {nome: 'Vianda', preco: '5,74', img: '../assets/img/food1.jpg'},
+    {nome: 'Feijoada', preco: '9,23', img: '../assets/img/food2.jpg'},
+    {nome: 'Carreteiro', preco: '12,31', img: '../assets/img/food3.jpg'},
+    {nome: 'Lasanha', preco: '11,90', img: '../assets/img/food1.jpg'},
+    {nome: 'Macarrão', preco: '13,95', img: '../assets/img/food2.jpg'},
+    {nome: 'Caldo Verde', preco: '21,83', img: '../assets/img/food3.jpg'},
+  ];
+
+
 }
